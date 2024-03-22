05af4ef17bfa2424da4f3ce1472cb9c9d3d27515
Salmansadiq809545 <151068261+Salmansadiq809545@users.noreply.github.com>
2024-03-22 21:25:03 +0530

    Create AutomateBashscript.sh

---
+++ AutomateBashscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE_NAME="salman8095/new"
+TAG="latest"
+CONTAINER_NAME="salmanproject"
+PORT_MAPPING="8084:8091"
+
+
+docker stop "$CONTAINER_NAME"
+docker rm "$CONTAINER_NAME"
+docker build -t "$IMAGE_NAME:$TAG" .
+docker run -itd -p "$PORT_MAPPING" --name "$CONTAINER_NAME" "$IMAGE_NAME"
